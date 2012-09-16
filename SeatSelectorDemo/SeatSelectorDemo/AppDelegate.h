035fac6b4155b665da6ee33665b705ef83b41191
Gaurav Srivastava <gauravsrivastava@Gauravs-MacBook-Pro.local>
2012-09-16 18:42:11 +0530

    Created the project

---
+++ SeatSelectorDemo/SeatSelectorDemo/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  SeatSelectorDemo
+//
+//  Created by Gaurav Srivastava on 16/09/12.
+//  Copyright (c) 2012 Gaurav Srivastava. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ SeatSelectorDemo/SeatSelectorDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SeatSelectorDemo
+//
+//  Created by Gaurav Srivastava on 16/09/12.
+//  Copyright (c) 2012 Gaurav Srivastava. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
